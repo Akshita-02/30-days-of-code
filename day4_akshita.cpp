a065bdc3a3e3e806f43bc42e79fc29fad0f67fb4
Akshita-02 <75935750+Akshita-02@users.noreply.github.com>
2022-04-29 19:26:05 +0530

    Add files via upload

---
+++ day4_akshita.cpp
@@ -0,0 +1,18 @@
+//to check whether number is even or odd:
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the number:"<<endl;
+    cin>>n;
+    if(n%2==0)
+        cout<<"The number which you entered is even"<<endl;
+        else
+        cout<<"The number which you entered is odd"<<endl;
+
+    return 0;
+}
