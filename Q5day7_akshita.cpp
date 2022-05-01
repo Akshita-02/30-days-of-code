1218771c596744c0006e82c6dd46a2f4da48e1b3
Akshita-02 <75935750+Akshita-02@users.noreply.github.com>
2022-05-01 13:28:29 +0530

    Add files via upload

---
+++ Q5day7_akshita.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+char x;
+cout<<"enter the alphabet:"<<endl;
+cin>>x;
+if((x=='a'||x=='e'||x=='i'||x=='o'||x=='i'||x=='u')||(x=='A'||x=='E'||x=='I'||x=='O'||x=='U'))
+cout<<"Vowel"<<endl;
+else 
+cout<<"Consonent"<<endl;
+}
