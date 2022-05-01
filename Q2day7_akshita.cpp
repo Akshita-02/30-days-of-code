256c9fba0efa3fdd90c2838e6ca809dcb4a80133
Akshita-02 <75935750+Akshita-02@users.noreply.github.com>
2022-05-01 12:57:13 +0530

    Add files via upload

---
+++ Q2day7_akshita.cpp
@@ -0,0 +1,18 @@
+//Program to find whether the given number is even number or odd number
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+
+    cout<<"enter the number:"<<endl;
+    cin>>a;
+if(a%2==0)
+cout<<"even number"<<endl;
+else
+cout<<"odd number"<<endl;
+    return 0;
+}
